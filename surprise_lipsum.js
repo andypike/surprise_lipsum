2d282d9db260139e82e7b7ed5aad7fd63d381a0d
Kyle Shevlin <kyle@finedesigngroup.com>
2014-08-14 10:02:20 -0700

    add random punctuation

--- surprise_lipsum.js
+++ surprise_lipsum.js
@@ -3,16 +3,34 @@ var surprise_lipsum = window.surprise_lipsum || {};
 surprise_lipsum.generator = function () {
     var latin = "lorem ipsum dolor sit amet consectetuer adipiscing elit nam cursus morbi ut mi nullam enim leo egestas id condimentum at laoreet mattis massa sed eleifend nonummy diam praesent mauris ante elementum et bibendum at posuere sit amet nibh duis tincidunt lectus quis dui viverra vestibulum suspendisse vulputate aliquam dui nulla elementum dui ut augue aliquam vehicula mi at mauris maecenas placerat nisl at consequat rhoncus sem nunc gravida justo quis eleifend arcu velit quis lacus morbi magna magna tincidunt a mattis non imperdiet vitae tellus sed odio est auctor ac sollicitudin in consequat vitae orci fusce id felis vivamus sollicitudin metus eget eros".split(" ");
 
+    var punctuation = ['.','!','?',',',';','-',':'];
+
+    function random_bool_w_bias() {
+      var x    = Math.random(),
+          bias = 0.75;
+
+      if (x < bias) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    function random_punctuation() {
+      var x = Math.floor(Math.random() * punctuation.length);
+      return punctuation[x];
+    }
+
     function random_int(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    
+
     function surprise_lipsum() {
       $("*").filter(function(){
         var match = $(this).text().match(/^\[lipsum (\d+)w (\d+)%\]/);
         if(match) {
           $(this).data("surprise-ideal", match[1]);
-          $(this).data("surprise-variance", match[2]);  
+          $(this).data("surprise-variance", match[2]);
         }
         return match;
       }).each(function() {
@@ -32,7 +50,12 @@ surprise_lipsum.generator = function () {
       var lipsum = "";
       for(var x = 0; x < words; x++) {
         index = random_int(0, latin.length - 1);
-        lipsum += latin[index] + " ";
+        lipsum += latin[index];
+        var bool = random_bool_w_bias();
+        if (bool) {
+          lipsum += random_punctuation();
+        }
+        lipsum += " ";
       }
 
       return lipsum;
